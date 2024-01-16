ac149b57231e0e625de13c9f6221d50a28ffa331
Lui-VEVO <122450740+Lui-VEVO@users.noreply.github.com>
2024-01-16 12:46:55 +0100

    Nye endringer i About.css og About.js
    Committer About.js, About.css,.

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,53 @@
 import React from 'react'
+import './About.css'
+import logoImage from "./images/lionGetAfterIt.png";
+import aboutBackExercise from "./images/TestBilde-Arm.jpg";
 
 const About = () => {
   return (
-    <div >
-      <h1>About</h1>
-   
+    <div className='container'>
+      <div className='d-flex justify-content-center'>
+      <h1>About us</h1>
+      </div>
+      
+
+      <div className='row'>
+        <div className='col-4'>
+          <img src={logoImage} alt="Logo" className="navbar-logo-about" />
+        </div>
+        <div className='col-8'>
+          <div className='row'>
+            <div className='col'>
+            <p>
+            In a world where health is wealth and every 'like', 'share', and 'follow' matters, we believe in the power of transformation, not just in our bodies but in our mindset. 
+            "Get After It" isn't just our name; it's our motto, our call to action, our promise to you. We are Jamal and Sander, the dynamic duo behind "Get After It". 
+            With years of combined experience, relentless passion, and an unyielding commitment to our craft, 
+            we've honed our expertise as personal trainers to bring you fitness solutions that truly make a difference. 
+
+          </p>
+            </div>
+          </div>
+
+          <div className='row'>
+            <div className='col'>
+            <p>We're not just here to sell; we're here to inspire, to motivate, and to guide. 
+            By merging our expertise with the accessibility of social media, we're revolutionizing how online training is perceived and executed.
+            So, whether you're a fitness enthusiast, someone just starting out, or anywhere in between, 
+            remember - every journey begins with a single step. Let us be your guides. Are you ready to Get After It? </p>
+            </div>
+          </div>
+          {/*
+          
+          </p> */}
+        </div>
+      </div>
+
+      <div className='row'>
+        <div className='col-8'>
+          <img src={aboutBackExercise} alt='backExercise' className='backExercise-image'/>
+        </div>
+      </div>
+
     </div>
   )
 }
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div ><h1>Contact us</h1>
+    <div >
+      <h1>Contact us</h1>
+      
     </div>
   )
 }
