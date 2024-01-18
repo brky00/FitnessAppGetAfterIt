7ee6fe2c09cad30679f8c608167dcc8ae7d9be56
Berkay Caglar <berokomozom@hotmail.com>
2024-01-18 14:05:04 +0100

    scard background

--- src/components/Shopping.js
+++ src/components/Shopping.js
@@ -1,36 +1,32 @@
 import React from "react";
-import './Shopping.css';
+import "./Shopping.css";
 import hoodie from "./images/hoodieGti.png";
 import short from "./images/shortGti.png";
 import ProductShoppingCard from "./elements/ProductShoppingCard";
 
-
-    // Örnek ürün verileri
-    const products = [
-     
-    ];
-    const newProduct = {
-       
-        imgSrc: hoodie,
-        size: 'L',
-        quantity: 1,
-        price: 150
-      };
-      const newProductsArray = [...products, newProduct];
-      const newProduct2 = {
-       
-        imgSrc: short,
-        size: 'L',
-        quantity: 1,
-        price: 150
-      };
-      const newProductsArray2 = [...newProductsArray, newProduct];
-
+// Örnek ürün verileri
+const products = [];
+const newProduct = {
+  imgSrc: hoodie,
+  size: "L",
+  quantity: 1,
+  price: 150,
+};
+const newProductsArray = [...products, newProduct];
+const newProduct2 = {
+  imgSrc: short,
+  size: "L",
+  quantity: 1,
+  price: 150,
+};
+const newProductsArray2 = [...newProductsArray, newProduct];
 
 const Shopping = () => {
   return (
+    
+    
     <div className="container sCardContainer">
-      <div >
+      <div>
         <div className="row text-center mb-2">
           <div className="col">Product</div>
           <div className="col">Size</div>
@@ -39,20 +35,20 @@ const Shopping = () => {
         </div>
 
         {/* Product */}
-        
+
         <ProductShoppingCard
-        imgSrc={newProduct.imgSrc}
-        size={newProduct.size}
-        quantity={newProduct.quantity}
-        price={newProduct.price}/>
+          imgSrc={newProduct.imgSrc}
+          size={newProduct.size}
+          quantity={newProduct.quantity}
+          price={newProduct.price}
+        />
 
-<ProductShoppingCard
-        imgSrc={newProduct2.imgSrc}
-        size={newProduct2.size}
-        quantity={newProduct2.quantity}
-        price={newProduct2.price}/>
-
-   
+        <ProductShoppingCard
+          imgSrc={newProduct2.imgSrc}
+          size={newProduct2.size}
+          quantity={newProduct2.quantity}
+          price={newProduct2.price}
+        />
 
         {/* Summary Row */}
         <div className="row">
@@ -79,9 +75,7 @@ const Shopping = () => {
         {/* Action Buttons */}
         <div className="row">
           <div className="col-md-2 offset-md-8 mb-3">
-            <button className="contiuneButton">
-              Continue Shopping
-            </button>
+            <button className="contiuneButton">Continue Shopping</button>
           </div>
           <div className="col-md-2">
             <button className="checkoutButton">Checkout</button>
@@ -93,4 +87,3 @@ const Shopping = () => {
 };
 
 export default Shopping;
-
