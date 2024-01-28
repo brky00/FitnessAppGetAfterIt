275c68a16120af83d82505305b2f329a238a71ed
Lui-VEVO <122450740+Lui-VEVO@users.noreply.github.com>
2024-01-28 14:03:30 +0100

    Fikset font og farger til sign in button

--- src/components/LoginAdmin.js
+++ src/components/LoginAdmin.js
@@ -1,22 +1,23 @@
 import React from 'react'
 import './LoginAdmin.css'
 
+
 const Login = () => {
     return(
         <div className="container-login">
             <form>
                 <h1>LOGIN AS ADMIN</h1>
-                <div class="form-group">
+                <div className="form-group">
                     <label for="exampleInputEmail1">Email address</label>
                     <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
                     
                 </div>
-                <div class="form-group">
+                <div className="form-group">
                     <label for="exampleInputPassword1">Password</label>
-                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password"/>
+                    <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password"/>
                 </div>
                 
-                <button type="submit" class="btn btn-primary">Sign in</button>
+                <button type="submit" className="btn-primary">Sign in</button>
                 <a href="/" className='back-link'>&larr;Back to home page</a>
             </form>
         </div>
