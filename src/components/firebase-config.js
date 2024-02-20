67c94f964fb710954fa3d7c867193cbd2e2c4343
Berkay Caglar <berokomozom@hotmail.com>
2024-02-20 13:05:16 +0100

    firebase-config,login

--- src/components/firebase-config.js
+++ src/components/firebase-config.js
@@ -25,15 +25,14 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 // Initialize Firebase Authentication and get a reference to the service
-export const db = getFirestore(app)
-export const storage = getStorage(app);
-const auth = getAuth(app);
 const db = getFirestore(app)
+const storage = getStorage(app);
+const auth = getAuth(app);
 const analytics = getAnalytics(app)
 
 
 // Export the Firebase services that you want to use
-export { auth, db, analytics };
+export { auth,storage, db, analytics };
 
 
 
