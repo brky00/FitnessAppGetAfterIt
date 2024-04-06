eed6dce7493e168b335dec9314fcebec6009fbed
Berkay Caglar <berokomozom@hotmail.com>
2024-04-06 13:22:50 +0200

    Dashboard

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,24 +1,10 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import "./Dashboard.css";
-import { db, analytics } from "../firebase-config";
-import Logout from "../Logout";
 
 import "./Dashboard.css";
 
-import { collection, query, where, getDocs } from "firebase/firestore";
-
-const InfoBox = ({ title, value }) => (
-  <div className="card text-center">
-    <div className="card-body">
-      <h5 className="card-title">{title}</h5>
-      <p className="card-text">{value}</p>
-    </div>
-  </div>
-);
-
 const Dashboard = () => {
-  const [userVisits, setUserVisits] = useState(0);
   const [userContacts, setUserContacts] = useState(0);
   const [orders, setOrders] = useState(0);
 
@@ -27,9 +13,9 @@ const Dashboard = () => {
       <div className="container-fluid dashBoardContainer">
         <div className="d-flex justify-content-end me-5"></div>
         <div className="row dashBoardRow">
-          <div className="col-4 col-sm-4 col-lg-2 leftSideCol mt-3">
-            <div className="row">
-              <div className="col-12 dashBoardProductsCol">
+          <div className="col-4 col-sm-4 col-md-4 col-lg-2 leftSideCol mt-3">
+            <div className="row mt-5">
+              <div className="col-12 dashBoardProductsCol mb-3">
                 <Link className="dashBoardProducts" to="/dashTable">
                   <div className="d-flex justify-content-center align-items-center productLinkInnerDiv">
                     {" "}
@@ -41,20 +27,23 @@ const Dashboard = () => {
                 </Link>
               </div>
 
-              <div className="col-12 dashBoardProductsCol">
-                <Link className="dashBoardProducts" to="/dashOrder">
+              <div className="col-12 dashBoardProductsCol mb-3">
+                <Link className="dashBoardOrder" to="/dashOrder">
                   <div className="d-flex justify-content-center align-items-center productLinkInnerDiv">
                     {" "}
                     <h2 className="me-3"> Order </h2>
                     <div>
-                      <i class="fa-solid fa-bag-shopping"></i>
+                      <i class="fa-solid fa-truck-fast"></i>
                     </div>
                   </div>
                 </Link>
               </div>
 
-              <div className="col-12 mt-5">
-                <Link className="Usercontact" to="/Usercontact">
+              <div className="col-12 ">
+                <Link
+                  className="Usercontact dashBoardUserContact"
+                  to="/Usercontact"
+                >
                   <div className="d-flex justify-content-center align-items-center productLinkInnerDiv">
                     <h2 className="me-3">Contact</h2>
                     <div>
@@ -62,6 +51,16 @@ const Dashboard = () => {
                     </div>
                   </div>
                 </Link>
+              </div>
+            </div>
+          </div>
+          <div className="col-8 col-sm-8 col-md-8 col-lg-8">
+            <h1 className="DashBoardTitle mt-3">Dashboard</h1>
+            <div class="container text-center DashBoardBody">
+              <div class="row align-items-start d-flex justify-content-center">
+                <div class="col-8 col-sm-2 col-md-2 col-lg-2 dashBodyCols ms-5 me-5">Users visited</div>
+                <div class="col-8 col-sm-2 col-md-2 col-lg-2  dashBodyCols me-5">Total orderes</div>
+                <div class="col-8 col-sm-2 col-md-2 col-lg-2  dashBodyCols me-5">Users contacted</div>
               </div>
             </div>
           </div>
